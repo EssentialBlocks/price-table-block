b2b0c46bb6996f08463cc8fcc5ff376320cd62ca
Tasnim Alam <tasnimalamcse@gmail.com>
2020-07-22 10:56:17 +0600

    Add icon

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '5a376e12b9b22d76b43c790757d1c870');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9c533543119bac660842ea048fc1427d');
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { __ } from "@wordpress/i18n";
 import "./style.scss";
 import Edit from "./edit";
 import save from "./save";
+import icon from "./icon";
 
 registerBlockType("essential-blocks/pricing-table", {
 	title: __("Pricing Table", "essential-blocks"),
 	description: __("", "essential-blocks"),
 	category: "widgets",
-	icon: "smiley",
+	icon,
 	edit: Edit,
 	save,
 });
