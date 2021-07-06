529d3701f73beaa188ff21c4a16a056e8f6884aa
Md. Monir Hossain <fencemonir@gmail.com>
2021-07-06 13:05:00 +0600

    update price tag css issue

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => '9185f7a92b12e66a4334c84c1799268f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => 'c788f0ee7472798cd3cdd1691a68eba6');
